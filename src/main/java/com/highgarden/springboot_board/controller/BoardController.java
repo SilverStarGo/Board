4c684868750679a17b0a7d1ffc33293de1367b11
Eun seong <8ohowb@naver.com>
2025-03-20 18:57:37 +0900

    상세조회 기능, 조회수

--- src/main/java/com/highgarden/springboot_board/controller/BoardController.java
+++ src/main/java/com/highgarden/springboot_board/controller/BoardController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -35,4 +36,13 @@ public class BoardController {
         return "list";
     }
 
+    @GetMapping("/{id}")
+    public String findById(@PathVariable("id")Long id, Model model){ //PathVariable : 자동으로 url요청을 변수에 담을 수 있다.
+        //조회수 처리
+        boardService.updateHits(id);
+        //상세내용 가져오기
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        return "detail";
+    }
 }
--- src/main/java/com/highgarden/springboot_board/repository/BoardRepository.java
+++ src/main/java/com/highgarden/springboot_board/repository/BoardRepository.java
@@ -21,4 +21,12 @@ public class BoardRepository {
         System.out.println("findAll");
         return sql.selectList("Board.findAll");
     }
+
+    public void updateHits(Long id){
+        sql.update("Board.updateHits", id);
+    }
+    public BoardDTO findById(Long id){ //조회수 기능은 반환값이 따로 필요없지만
+        // 상세조회는 글내용에 대한 데이터를 반환해야 하기 때문에 sql.selectOne으로 데이터를 받아 BoardDTO객체에 담아 반환
+        return sql.selectOne("Board.findById", id);
+    }
 }
